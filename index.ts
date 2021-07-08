cb08341c20ec74718ccdf9b3fd328878f230b24c
ncitron <noah@jeff.org>
2021-07-08 16:18:37 -0400

    use web3

--- index.ts
+++ index.ts
@@ -1,16 +1,18 @@
-import { ethers } from "ethers";
+import Web3 from "web3";
 const fliABI = require("./fliViewerABI.json");
 
 require("dotenv").config();
 
 async function run() {
-  const provider = ethers.providers.getDefaultProvider(process.env.JSON_RPC_URL);
-  const viewer = new ethers.Contract("0xFDa5AE4d7d466f466735B9AEc6eA83841260FDC7", fliABI, provider);
 
-  const result = await viewer.callStatic.shouldRebalanceWithBounds(
-    ethers.utils.parseEther("1.8"),
-    ethers.utils.parseEther("2.2")
-  );
+  const web3 = new Web3(process.env.JSON_RPC_URL || "");
+
+  const viewer = new web3.eth.Contract(fliABI, "0xFDa5AE4d7d466f466735B9AEc6eA83841260FDC7");
+
+  const result = await viewer.methods.shouldRebalanceWithBounds(
+    web3.utils.toWei("1.8"),
+    web3.utils.toWei("2.2")
+  ).call();
 
   console.log(result);
 }
